e7f108cab54b9c4706961ac0b5037e701fa5026c
Hugo <carloshugolopes@gmail.com>
2018-10-18 20:27:44 -0300

    projeto iniciado para aprendizado do c#

---
+++ primeiro-projeto/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace primeiro_projeto {
+    class Produto {
+        public string nome;
+        public double preco;
+        public int quantidadeEmEstoque;
+
+        public double valorTotalEmEstoque() {
+            return preco * quantidadeEmEstoque;
+        }
+
+        public void realizarEntrada(int quantidade) {
+            quantidadeEmEstoque += quantidade;
+        }
+
+        public void realizarSaida(int quantidade) {
+            quantidadeEmEstoque -= quantidade;
+        }
+
+        public override string ToString() {
+            return nome 
+                + ", R$" 
+                + preco.ToString("F2", CultureInfo.InvariantCulture) 
+                +", "
+                + quantidadeEmEstoque
+                + " unidades, Total: R$" 
+                + valorTotalEmEstoque();
+        }
+    }
+}
---
+++ primeiro-projeto/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace primeiro_projeto {
+    class Program {
+        static void Main(string[] args) {
+            Produto P;
+            P = new Produto();
+            int qte;
+
+            Console.WriteLine("Digite os dados do produto");
+            Console.Write("Nome: ");
+            P.nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            P.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            P.quantidadeEmEstoque = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + P.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a quantidade de produtos entraram no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            P.realizarEntrada(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + P.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a quantidade de produtos saíram no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            P.realizarSaida(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + P.ToString());
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
